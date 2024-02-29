81ba3ef847c2a7151fd52ff39c6c8f778274f904
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-29 17:29:36 +0000

    Creating the variables for the movie class

---
+++ src/main/java/com/example/movies/Movie.java
@@ -0,0 +1,29 @@
+package com.example.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data // this takes care of the getters and setters methods
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    // variables
+    @Id
+    private ObjectId id;
+    private String imdbID;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+
+}
