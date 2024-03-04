0a4fbf96c60accce71402ec3d30ac312340b599d
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-04 08:45:12 +0000

    adding methods to get all the movies

--- src/main/java/com/example/movies/MovieService.java
+++ src/main/java/com/example/movies/MovieService.java
@@ -2,6 +2,14 @@ package com.example.movies;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MovieService {
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+
+    }
+
 }
