709f6e903a0ce5381be2ea238142773ba372c224
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-12 18:38:45 +0000

    creating hero compoent

--- movie-gold-v1/src/App.js
+++ movie-gold-v1/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import api from './api/axiosConfig'; 
 api.defaults.baseURL = 'http://localhost:8080';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/home/Home';
 
 function App() {
   const [Movies, setMovies] = useState(); 
@@ -24,6 +27,12 @@ function App() {
 
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout/>}> 
+        <Route path="/" element={<Home/>}></Route>
+
+        </Route>
+      </Routes>
     
     </div>
   );
